45055bcf86f3708399011552b5a16982c10a29e3
Shimbhu77 <kumawatshimbhu4@gmail.com>
2024-07-04 23:36:20 +0530

    project started

---
+++ quark-stream/src/test/java/com/quarkstream/api/QuarkStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quarkstream.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuarkStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
